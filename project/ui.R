f06b26415622a44a6e80253e4e30a7c4ac822021
Bioticcc <adamward.bio@gmail.com>
2025-09-07 18:34:27 -0700

    added second tabPanel

--- project/ui.R
+++ project/ui.R
@@ -1,7 +1,9 @@
 #ui.R
 
 
-#setting a theme:
+# ui.R
+
+# setting a theme
 theme <- bslib::bs_theme(
   version   = 5,
   bootswatch = "minty",
@@ -11,34 +13,46 @@ theme <- bslib::bs_theme(
   fg        = "#2C3E50"
 )
 
-
-
-#THE MAIN UI SECTION
-ui <- tagsList(
-  #loading style.css
+# MAIN UI SECTION
+ui <- tagList(
+  # load style.css
   tags$head(
-      tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
-      tags$script(HTML("console.log('Custom CSS loaded!');"))
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css"),
+    tags$script(HTML("console.log('Custom CSS loaded!');"))
   ),
   
   navbarPage(
-  title    = "",
-  id       = "menu",
-  selected = "home",
-  theme    = theme, 
+    title    = "",
+    id       = "menu",
+    selected = "home",
+    theme    = theme, 
 
-  # tabs
-  tabPanel(
-    title = "Home",
-    value = "home",
-    tags$div(
-      class = "header-box",
-      style = "display: flex; align-items: center; gap: 20px; margin-bottom: 20px;",
+    # Home tab
+    tabPanel(
+      title = "Home",
+      value = "home",
       tags$div(
-        style = "display: flex; flex-direction: column; gap: 6px;",
-        tags$h1("Rate My Prof (WSU)")
+        class = "header-box",
+        style = "display: flex; align-items: center; gap: 20px; margin-bottom: 20px;",
+        tags$div(
+          style = "display: flex; flex-direction: column; gap: 6px;",
+          tags$h1("Rate My Prof (WSU)")
+        )
+      )
+    ),
+
+    # Authentication tab
+    tabPanel(
+      title = "Authentication",
+      value = "auth",
+      tags$div(
+        class = "header-box",
+        style = "display: flex; align-items: center; gap: 20px; margin-bottom: 20px;",
+        tags$div(
+          style = "display: flex; flex-direction: column; gap: 6px;",
+          tags$h1("Submit WSU ID in order to publish reviews")
+        )
       )
     )
   )
-)
 )
